9b62a6eb7fc281ae2beeda288d306b1cd83cd6b9
mertess <mitya.lagin@yandex.ru>
2020-11-11 17:47:24 +0400

    init

---
+++ Web-service/Controllers/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_service.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet]
+        public string GetImages(string url, int threadCount, int imageCount)
+        {
+            return "hah";
+        }
+    } 
+}
---
+++ Web-service/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Web_service
+{
+    [DataContract]
+    public class Test
+    {
+        [DataMember]
+        public string TestStr { get; set; }
+    }
+}
